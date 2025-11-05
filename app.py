d18a027fa26709dbe97b81427e9e81fddf2fef83
Fernandinho Beira Mar <borabillescola@gmail.com>
2025-11-05 15:24:14 -0300

    API RESTful desenvolvida em Python com Flask

---
+++ app.py
@@ -0,0 +1,81 @@
+# 1. Instalamos a biblioteca flask no terminal com o comando: pip install flask
+# 2. Importamos a biblioteca flask para o nosso projeto
+from flask import Flask, make_response, jsonify, request
+from bd_livros import livros
+
+# Bibliotecas novas
+# jsonify = lista, dicionários em arquivos json
+# make_response = cria um objeto de resta HTTP completo, ele não apenas retorna, mas também permite personalizar cabeçalhos e status code. Ela vai transformar o JSON em resposta HTTP.
+# O jsonify funciona sem o make_response, usamos ele apenas para mais controle sobre a resposta, ou seja, para obter outros status fora o 200.
+
+# 3. Instanciamos o Flask (criar um objeto a partir de uma classe)
+# (Uma instancia é o objeto real counstruído a partir desse molde)
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS']= False
+
+@app.route('/livros', methods=['GET'])
+def get_livros():
+    return make_response(jsonify(
+        mensagem = 'Lista de Livros',
+        dados = livros
+
+    ))
+
+# GET - Buscar livro por ID
+@app.route('/livros/<int:id>', methods=['GET'])
+def get_livro(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return make_response(jsonify(
+               mensagem = 'Livro Encontrado', dados = livro
+            ))
+    return make_response(jsonify(mensagem='Livro Não Encontrado'), 404)
+
+# POST - Criar um novo livro
+@app.route('/livros', methods=['POST'])
+def create_livro():
+    livro = request.json
+    livros.append(livro)
+    return make_response(jsonify(mensagem='Novo Livro adicionado com sucesso', dados=livro), 201)
+
+# PUT - Atualizar livro (substitui todos os dados)
+@app.route("/livros/<int:id>", methods=['PUT'])
+def update_livro(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            novo_livro = request.json
+            livro.update(novo_livro)
+            return make_response(jsonify(mensagem=f'Livro id {id} atualizado com sucesso (PUT)',
+                                         dados=livro))
+    return make_response(jsonify(mensagem='Livro não encontrado'), 404)
+
+# PATCH - Atualizar parcialmente um livro
+@app.route('/livros/<int:id>', methods=['PATCH'])
+def patch_livro(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            dados = request.json
+            livro.update(dados)
+            return make_response(jsonify(
+                mensagem=f'Livro ID {id} atualizado com sucesso.',
+                dados=livro
+            ), 200)
+    return make_response(jsonify(
+        mensagem='Livro não encontrado.'), 404)
+
+# DELETE - Remover um livro
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def delete_livro(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            livros.remove(livro)
+            return make_response(jsonify(
+                mensagem=f'Livro ID {id} removido com sucesso.',
+                dados=livro
+            ), 200)
+    return make_response(jsonify(
+        mensagem='Livro não encontrado'
+    ), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ bd_livros.py
@@ -0,0 +1,15 @@
+# Criamos uma lista com 10 dicionários. Cada dicionário traz as informações sobre um livro.
+# Essa lista fará o papel de 'banco de dados' no nosso projeto.
+
+livros = [
+    {'id':1,'titulo':'O Pequeno Principe','autor':'Saint-Exupéry','ano':1943},
+    {'id':2,'titulo':'1984','autor':'George Orwell','ano':1949},
+    {'id':3,'titulo':'Fahrenheit 451','autor':'Ray Bradbury','ano':1953},
+    {'id':4,'titulo':'Brave New World','autor':'Aldous Huxley','ano':1932},
+    {'id':5,'titulo':'The Catcher in the Rye','autor':'J.D. Salinger','ano':1951},
+    {'id':6,'titulo':'To Kill a Mockingbird','autor':'Harper Lee','ano':1960},
+    {'id':7,'titulo':'The Great Gatsby','autor':'F. Scott Fitzgerald','ano':1925},
+    {'id':8,'titulo':'Moby Dick','autor':'Herman Melville','ano':1851},
+    {'id':9,'titulo':'O Morro dos Ventos Uivantes','autor':'Emily Brontë','ano':1847},
+    {'id':10,'titulo':'Orgulho e Preconceito','autor':'Jane Austen','ano':1813}
+]
